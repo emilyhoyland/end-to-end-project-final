4943b2fe971b0c0b8e719745ed334fbb07986f19
AndrewCooley1 <ac8896@stern.nyu.edu>
2021-08-08 11:53:54 -0400

    Testing

---
+++ app/test.py
@@ -0,0 +1,12 @@
+import flair
+sentiment_model_fl = flair.models.TextClassifier.load('en-sentiment')
+#title = input("enter a sentence to check its sentiment score: ")
+def sentiment_test(title):
+   
+    each_title = flair.data.Sentence(title)
+    sentiment_model_fl.predict(each_title)
+    return(each_title.labels[0].value, each_title.labels[0].score)
+
+if __name__ == "__main__":
+    title = input("enter a sentence to check its sentiment score: ")
+    sentiment_test(title)
