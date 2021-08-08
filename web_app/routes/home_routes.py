ba7705676ec636131416b21bdd2a2b2511d59419
vivekagarwal14 <86423179+vivekagarwal14@users.noreply.github.com>
2021-08-07 22:53:04 -0400

    update

--- web_app/routes/home_routes.py
+++ web_app/routes/home_routes.py
@@ -4,14 +4,26 @@ from flask import Blueprint, request, render_template
 
 home_routes = Blueprint("home_routes", __name__)
 
-@home_routes.route('/')
+@home_routes.route("/")
 @home_routes.route("/home")
 
 def index():
     print("HOME...")
-    return "Welcome Home to the ticker sentiment app"
+    #return "Welcome Home to the ticker sentiment app"
+    return render_template("home.htm")
+
 
 @home_routes.route("/about")
 def about():
     print("About...")
-    return "About Me - I can tell you the sentiment for past few days"
+    #return "About Me - I can tell you the sentiment for past few days"
+    return render_template("about.htm")
+
+@home_routes.route("/hello")
+def hello_world():
+    print("Welcome....", dict(request.args))
+    name = request.args.get("name") or "Investor to our App."
+    message = f"Hello, {name}!"
+    # return message
+    return render_template('hello.htm', message=message)
+
--- web_app/routes/stocksentiment_routes.py
+++ web_app/routes/stocksentiment_routes.py
@@ -1,6 +1,6 @@
 
 
-from flask import Blueprint, request, jsonify, render_template
+from flask import Blueprint, request, jsonify, render_template, redirect
 
 from app.stock_sentiment import lookup_ticker
 
@@ -19,4 +19,32 @@ def stock_sentiment_api():
         return jsonify(results)
     else:
         return jsonify({"message":"Invalid ticker or stock name. Please try again"}), 404
-    
+
+
+@stocksentiment_routes.route("/stock/form")
+def stock_form():
+    print("STOCK FORM...")
+    return render_template("stockticker_form.htm")
+
+
+@stocksentiment_routes.route("/stock/sentiment", methods=["GET", "POST"])
+def stocksentiment_score():
+    print("STOCK SENTIMENT SCORE...")
+
+    if request.method == "GET":
+        print("URL PARAMS:", dict(request.args))
+        request_data = dict(request.args)
+    elif request.method == "POST": # the form will send a POST
+        print("FORM DATA:", dict(request.form))
+        request_data = dict(request.form)
+
+    ticker = request_data.get("ticker") or "AMZN"
+    #zip_code = request_data.get("zip_code") or "20057"
+
+    results = lookup_ticker(ticker=ticker)
+    if results:
+        #flash("Weather Forecast Generated Successfully!", "success")
+        return render_template("stock_sentiment.htm", ticker=ticker, results=results)
+    else:
+        #flash("Geography Error. Please try again!", "danger")
+        return redirect("/stock/form")
