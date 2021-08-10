f34d7adbcdbb0b17d7a9fe7cd262d9914549432e
emilyhoyland <ech435@nyu.edu>
2021-08-09 21:02:01 -0400

    Chart

--- app/stock_chart.py
+++ app/stock_chart.py
@@ -94,7 +94,7 @@ if __name__ == "__main__":
 
 
     #create a visualisation for the user to see the trend
-    median_df_fl1['final_confidence_fl'].plot(lw=2, colormap='jet', marker='.', markersize=10, title='Sentiment Trend Analysis of Stocks')
+    median_df_fl1['final_confidence_fl'].plot(lw=2, colormap='jet', marker='.', markersize=10, title='Daily Median Sentiment Analysis for Stock')
     plt.xlabel('Dates for most recent 100 news articles')
     plt.ylabel('Median Sentiment Score')
 
